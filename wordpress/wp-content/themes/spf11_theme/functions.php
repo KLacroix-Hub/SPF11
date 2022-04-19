21bff146e72bfc5970d9d9250d564ff10ab2cf6f
Kévin Lacroix <kevin.lacroix@hetic.net>
2022-04-19 08:57:18 +0200

    Init produit

---
+++ wordpress/wp-content/themes/spf11_theme/functions.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ */
+
+/*
+ * Let WordPress manage the document title.
+ */
+add_theme_support( 'title-tag' );
+
+/*
+ * Enable support for Post Thumbnails on posts and pages.
+ */
+add_theme_support( 'post-thumbnails' );
+
+/*
+ * Switch default core markup for search form, comment form, and comments
+ * to output valid HTML5.
+ */
+add_theme_support( 'html5', array(
+	'search-form',
+	'comment-form',
+	'comment-list',
+	'gallery',
+	'caption',
+) );
+
+/** 
+ * Include primary navigation menu
+ */
+function untheme_nav_init() {
+	register_nav_menus( array(
+		'menu-1' => 'Primary Menu',
+		'menu-2' => 'Account menu',
+	) );
+}
+add_action( 'init', 'untheme_nav_init' );
+
+/**
+ * Register widget area.
+ *
+ */
+function untheme_widgets_init() {
+	register_sidebar( array(
+		'name'          => 'Sidebar',
+		'id'            => 'sidebar-1',
+		'description'   => 'Add widgets',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'untheme_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function untheme_scripts() {
+	wp_enqueue_style( 'untheme-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'untheme-custom-style', get_template_directory_uri() . '/assets/css/style.css' );
+	wp_enqueue_script( 'untheme-scripts', get_template_directory_uri() . '/assets/js/scripts.js' );
+}
+add_action( 'wp_enqueue_scripts', 'untheme_scripts' );
+
+add_filter('use_block_editor_for_post', '__return_false', 10);
+
+
+add_filter('woocommerce_account_menu_items', 'QuadLayers_remove_acc_address', 9999);
+
+function QuadLayers_remove_acc_address( $items ) {
+unset( $items['downloads'], $items['payment-methods'] );
+return $items;
+}
