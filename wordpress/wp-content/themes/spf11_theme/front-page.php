4988e2b1ddd6a903de606a7e82e32ed9bf9863f6
KLacroix-Hub <kevin.lacroix@heaven.fr>
2022-04-21 12:01:59 +0200

    Update style

--- wordpress/wp-content/themes/spf11_theme/front-page.php
+++ wordpress/wp-content/themes/spf11_theme/front-page.php
@@ -4,6 +4,7 @@ get_header();?>
 
     <div class="container-main">
         <div class="container__hero-banner">
+            <div class="container__hero-banner__black"></div>
 
             <?php if( get_field('home_banner') ): ?>
                 <img src="<?php the_field('home_banner'); ?>" />
@@ -12,13 +13,24 @@ get_header();?>
         
             <?php endif; ?>
 
+            <div class="container__hero-banner__center">
+                <?php if( get_field('home_banner-txt') ): ?>
+                    <div class="container__hero-banner__center-txt"><?php the_field('home_banner-txt'); ?></div>
+                <?php else:?>
+                    <div class="container__hero-banner__center-notxt">XXX</div>  
+            
+                <?php endif; ?>
+    
+                <a class="container__hero-banner__center-link" href="/boutique">Découvrir nos produits</a>
+            </div>
+
         </div>
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         
         <div class="container-category">
             <h1 class="container-category__title">Nos catégories</h1>
-            <a href="" class="container-category__link"></a>
+            <a href="" class="container-category__link">Voir toutes les catégories</a>
             <div class="container-category__products">
                 <?php echo do_shortcode('[product_categories limit="4" orderby="name" order="ASC]'); ?>
             </div>
