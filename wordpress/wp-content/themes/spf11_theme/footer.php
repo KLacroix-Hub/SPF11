6d635dad05f9de01e54b82f302a5851af61a5a32
KLacroix-Hub <kevin.lacroix@heaven.fr>
2022-04-21 18:04:16 +0200

    Update style footer

--- wordpress/wp-content/themes/spf11_theme/footer.php
+++ wordpress/wp-content/themes/spf11_theme/footer.php
@@ -11,26 +11,26 @@
 <footer class="site-footer">
     <div class="container-paiements">
         <h2>Paiements sécurisés</h2>
-        <div>
-            <img src="" alt="">
-            <img src="" alt="">
-            <img src="" alt="">
+        <div class="container-paiements__logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="Paypal">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" alt="Visa">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="MasterCard">
         </div>
     </div>
     <div class="container-mentions">
         <h2>Mentions légales</h2>
-        <div>
-            <a href="">Mentions légales</a>
-            <a href="">Conditions générales de ventes</a>
-            <a href="">Conditions générales d'utilisations</a>
+        <div class="container-mentions__list">
+            <a href="/mentions-legales">Mentions légales</a>
+            <a href="/politique-de-confidentialite">Conditions générales de ventes</a>
+            <a href="/conditions-utilisations">Conditions générales d'utilisations</a>
         </div>
     </div>
-    <div class="contact">
+    <div class="container-contact">
         <h2>Nous contacter</h2>
-        <div>
-            <a href="tel:<?php echo the_field("footer_tel");?>"><?php echo the_field("footer_tel"); ?></a>
-            <a href="mailto:<?php echo the_field("footer_mail");?>"><?php echo the_field("footer_mail"); ?></a>
-            <a href=""><?php echo the_field("footer_insta"); ?></a>
+        <div class="container-contact__list">
+            <a href="tel:<?php echo the_field("footer_tel");?>">Téléphone : <?php echo the_field("footer_tel"); ?></a>
+            <a href="mailto:<?php echo the_field("footer_mail");?>">Mail : <?php echo the_field("footer_mail"); ?></a>
+            <a href="">Instagram : <?php echo the_field("footer_insta"); ?></a>
         </div>
     </div>
 </footer>
