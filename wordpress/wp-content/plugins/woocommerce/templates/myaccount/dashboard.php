17722812c08f9c2c5bf83ba14e006b8d4f4aa3ba
kevinL <kevin.lacroix@hetic.net>
2022-05-04 11:17:17 +0200

    update

--- wordpress/wp-content/plugins/woocommerce/templates/myaccount/dashboard.php
+++ wordpress/wp-content/plugins/woocommerce/templates/myaccount/dashboard.php
@@ -28,21 +28,12 @@ $allowed_html = array(
 );
 ?>
 
-<div>
-	<p>
-	<?php
-	printf(
-		/* translators: 1: user display name 2: logout url */
-		wp_kses( __( 'Bonjour %1$s)', 'woocommerce' ), $allowed_html ),
-		wp_kses(__( '(not %1$s? <a href="%2$s">Log out</a>','woocommerce'), $allowed_html )
-		'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
-		esc_url( wc_logout_url() )
-	);
-	?>
-	</p>
+<div class="container-top">
+	<div class="container-top__hello">Bonjour <?php echo( $current_user->display_name ) ?></div>
+	<div class="container-top__logout">(Vous n'êtes pas <?php echo ($current_user->display_name) ?> ? <a href="http://example.com/wp-login.php?action=logout">Déconnexion</a>)</div>
 </div>
 
-<p>
+<p class="container-bottom">
 	<?php
 	/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
 	$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
